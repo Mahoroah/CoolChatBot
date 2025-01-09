d05814e2538a664e48bc154d75115806bee9d36b
Will Rages <rageswill@pleasval.org>
2025-01-09 08:47:34 -0600

    read all the stuff from the files intoa nr aryr

--- WordProcessor.java
+++ WordProcessor.java
@@ -4,11 +4,41 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.regex.*;
 public class WordProcessor {
-
     Scanner scan = new Scanner(System.in);
     Pattern pattern;
     Matcher matcher;
 
+    ArrayList<WordPattern> patterns;
+
+    public WordProcessor() {
+        File flagFile = new File("TextFiles/flags.txt");
+        Scanner flagReader = new Scanner(flagFile);
+        File responseFile = new File("TextFiles/response.txt");
+        Scanner responseReader = new Scanner(responseReader);
+
+        ArrayList<String> flags;
+        ArrayList<String> responses;
+
+        // read flag reader into an array
+        while (flagReader.hasNext()) {
+            String line = flagReader.nextLine();
+            flags.add(line);
+        }
+
+        while (responseReader.hasNext()) {
+            String line = responseReader.nextLine();
+            responses.add(line);
+        }
+
+        if (flags.size() != responses.size()) {
+            System.err.println("you suck");
+            System.exit(1);
+        }
+
+        for (i=0; i < flags.size(); i++) {
+            patterns.add(new WordPattern(flags.get(i), responses.get(i)));
+        }
+    }
 
     /**
      * This is the method for the chatbot to identify keywords that abide by regex Identifiers in the text file
@@ -33,7 +63,7 @@ public class WordProcessor {
             valid = matcher.find();
             counter++;
             if(valid) {
-            flags.add(counter);
+                flags.add(counter);
             }
         }
 
